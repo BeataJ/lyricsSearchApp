b664be81444adb05bd7bd25d981361e9da174c16
beata <beata@emc2wave.com>
2020-06-12 12:57:06 -0700

    create showData()

--- script.js
+++ script.js
@@ -15,10 +15,28 @@ async function searchSongs(term) {
   const data = await res.json();
 
   showData(data);
+  console.log(data);
 }
 
 // Show song and artist in DOM
-function showData(data) {}
+function showData(data) {
+  let output = '';
+
+  data.data.forEach((song) => {
+    output += `
+      <li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      </li>
+    `;
+  });
+
+  result.innerHTML = `
+    <ul class="songs">
+      ${output}
+    </ul>
+  `;
+}
 
 // Event listeners
 form.addEventListener('click', (e) => {
